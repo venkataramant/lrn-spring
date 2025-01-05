fe62d608ec8fe324e68f9774856e5be65aa6c671
Raman TVR <venkataramant@gmail.com>
2025-01-04 17:52:47 -0800

    fixing bug by Adding @EntityScan and @EnableJPaRepositories

--- spring-data/src/main/java/tvr/lrn/boot/ms/app/db/MSDaoEntityManagerApp.java
+++ spring-data/src/main/java/tvr/lrn/boot/ms/app/db/MSDaoEntityManagerApp.java
@@ -5,28 +5,31 @@ import java.util.Scanner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import tvr.lrn.boot.dao.SavingAccountJPADAO;
 import tvr.lrn.boot.entity.SavingAccountEntity;
 
 @SpringBootApplication
 @ComponentScan({ "tvr.lrn.boot", "tvr.lrn.boot.entity" })
-//@EnableJpaRepositories(basePackages = "tvr.lrn.boot.repository")
+@EntityScan(basePackageClasses = { SavingAccountEntity.class })
+@EnableJpaRepositories(basePackages = "tvr.lrn.boot.repository")
 public class MSDaoEntityManagerApp {
 
 	public static void main(String[] args) {
 		ApplicationContext ac = SpringApplication.run(MSDaoEntityManagerApp.class, args);
-		ac.getBean(SavingAccountEntity.class);
+//		ac.getBean(SavingAccountEntity.class);
 	}
 
 	@Bean
 	public CommandLineRunner run(SavingAccountJPADAO jpa) {
 		return runner -> {
 //			var savingAccount = createSavingAccount(jpa);
-			findSA(jpa, 2);
+			findSA(jpa, 4);
 			System.out.println("Application is running");
 		};
 	}
